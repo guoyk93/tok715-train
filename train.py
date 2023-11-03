6909b611db73efdf9c10e23cb2e599cc4b0c9849
GUO YANKE <hi@guoyk.xyz>
2023-11-03 17:59:36 +0800

    feat: load model and tokenizer

--- train.py
+++ train.py
@@ -1,8 +1,14 @@
+import click
+from transformers import BloomTokenizerFast, BloomForCausalLM
+
 BASE_MODEL_NAME = 'Langboat/bloom-800m-zh'
 
 
-def main():
-    pass
+@click.command()
+@click.option('--base_model', 'opt_base_model', default=BASE_MODEL_NAME, help='The base model to train.', type=str)
+def main(opt_base_model: str):
+    tokenizer = BloomTokenizerFast.from_pretrained(opt_base_model)
+    model = BloomForCausalLM.from_pretrained(opt_base_model)
 
 
 if __name__ == '__main__':
